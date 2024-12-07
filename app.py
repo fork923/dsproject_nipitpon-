906b28d6a99c21c98ceae72bf73947f7921e6081
fork923 <nipitponsuttluck@gmail.com>
2024-12-07 10:56:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -25,8 +25,6 @@ if page == "Homepage":
 if page == "BMI Calculator":
     with st.form("contact_form"):
         name = st.text_input("Your Name")
-        email = st.text_input("Your Email")
-        message = st.text_area("Your Message")
         submitted = st.form_submit_button("Submit")
 
 
