8b68d5dbfc78f1ebb152ffe10c027251f138ec43
fork923 <nipitponsuttluck@gmail.com>
2024-12-07 10:32:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="Portfolio App", layout="wide")
 
 # Sidebar navigation
 st.sidebar.title("Navigation")
-page = st.sidebar.radio("Go to", ["Homepage", "Contact Us"])
+page = st.sidebar.radio("Go to", ["Homepage", "BMI Calculator", "Contact Us"])
 
 # Homepage
 if page == "Homepage":
@@ -22,6 +22,90 @@ if page == "Homepage":
 
    I hope to receive a response from the Srinakharinwirot university !
     """)
+    if page == "BMI Calculator":
+        # import the streamlit library
+import streamlit as st
+
+# give a title to our app
+st.title('Welcome to BMI Calculator')
+# Text Input
+
+# save the input text in the variable 'name'
+# first argument shows the title of the text input box
+# second argument displays a default text inside the text input area
+name = st.text_input("Enter Your name", "Type Here ...")
+
+# display the name when the submit button is clicked
+# .title() is used to get the input text string
+if(st.button('Submit')):
+    result = name.title()
+    st.success(result)
+    # slider
+
+# first argument takes the title of the slider
+# second argument takes the starting of the slider
+# last argument takes the end number
+old = st.slider("Select the old", 1, 100)
+
+# print the level
+# format() is used to print value 
+# of a variable at a specific position
+st.text('Selected: {}'.format(level))
+
+# TAKE WEIGHT INPUT in kgs
+weight = st.number_input("Enter your weight (in kgs)")
+
+# TAKE HEIGHT INPUT
+# radio button to choose height format
+status = st.radio('Select your height format: ',
+                  ('cms', 'meters', 'feet'))
+
+# compare status value
+if(status == 'cms'):
+    # take height input in centimeters
+    height = st.number_input('Centimeters')
+
+    try:
+        bmi = weight / ((height/100)**2)
+    except:
+        st.text("Enter some value of height")
+
+elif(status == 'meters'):
+    # take height input in meters
+    height = st.number_input('Meters')
+
+    try:
+        bmi = weight / (height ** 2)
+    except:
+        st.text("Enter some value of height")
+
+else:
+    # take height input in feet
+    height = st.number_input('Feet')
+
+    # 1 meter = 3.28
+    try:
+        bmi = weight / (((height/3.28))**2)
+    except:
+        st.text("Enter some value of height")
+
+# check if the button is pressed or not
+if(st.button('Calculate BMI')):
+
+    # print the BMI INDEX
+    st.text("Your BMI Index is {}.".format(bmi))
+
+    # give the interpretation of BMI index
+    if(bmi < 16):
+        st.error("You are Extremely Underweight")
+    elif(bmi >= 16 and bmi < 18.5):
+        st.warning("You are Underweight")
+    elif(bmi >= 18.5 and bmi < 25):
+        st.success("Healthy")
+    elif(bmi >= 25 and bmi < 30):
+        st.warning("Overweight")
+    elif(bmi >= 30):
+        st.error("Extremely Overweight")
 
 
 # Contact Us
