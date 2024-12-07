28509cbc6d69c102a71d16b6c9bc1a152933f009
fork923 <nipitponsuttluck@gmail.com>
2024-12-07 11:04:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ if page == "BMI Calculator":
     with st.form("contact_form"):
         name = st.text_input("Enter Your Name")
          submitted = st.form_submit_button("Submit")
-        if submitted:
+          if submitted:
             st.success(name)
         age = st.text_input("Enter Your age")
         
