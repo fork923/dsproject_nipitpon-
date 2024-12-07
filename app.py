0c92e38b751ee462568752c7cf1f4972809c6c91
fork923 <nipitponsuttluck@gmail.com>
2024-12-07 11:16:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -47,47 +47,47 @@ if page == "BMI Calculator":
     #
         height = st.number_input('Centimeters')
 
-    try:
-        bmi = weight / ((height/100)**2)
-    except:
-        st.text("Enter some value of height")
+            try:
+                bmi = weight / ((height/100)**2)
+            except:
+                st.text("Enter some value of height")
 
-elif(status == 'meters'):
+    elif(status == 'meters'):
     # take height input in meters
     height = st.number_input('Meters')
 
-    try:
-        bmi = weight / (height ** 2)
-    except:
-        st.text("Enter some value of height")
+            try:
+                bmi = weight / (height ** 2)
+            except:
+                st.text("Enter some value of height")
 
-else:
+    else:
     # take height input in feet
-    height = st.number_input('Feet')
+        height = st.number_input('Feet')
 
     # 1 meter = 3.28
-    try:
-        bmi = weight / (((height/3.28))**2)
-    except:
-        st.text("Enter some value of height")
+            try:
+              bmi = weight / (((height/3.28))**2)
+            except:
+                st.text("Enter some value of height")
 
 # check if the button is pressed or not
-if(st.button('Calculate BMI')):
-
-    # print the BMI INDEX
-    st.text("Your BMI Index is {}.".format(bmi))
-
-    # give the interpretation of BMI index
-    if(bmi < 16):
-        st.error("You are Extremely Underweight")
-    elif(bmi >= 16 and bmi < 18.5):
-        st.warning("You are Underweight")
-    elif(bmi >= 18.5 and bmi < 25):
-        st.success("Healthy")
-    elif(bmi >= 25 and bmi < 30):
-        st.warning("Overweight")
-    elif(bmi >= 30):
-        st.error("Extremely Overweight")
+    if(st.button('Calculate BMI')):
+        
+            # print the BMI INDEX
+            st.text("Your BMI Index is {}.".format(bmi))
+        
+            # give the interpretation of BMI index
+            if(bmi < 16):
+                st.error("You are Extremely Underweight")
+            elif(bmi >= 16 and bmi < 18.5):
+                st.warning("You are Underweight")
+            elif(bmi >= 18.5 and bmi < 25):
+                st.success("Healthy")
+            elif(bmi >= 25 and bmi < 30):
+                st.warning("Overweight")
+            elif(bmi >= 30):
+                st.error("Extremely Overweight")
 
 
 # Contact Us
